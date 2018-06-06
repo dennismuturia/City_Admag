464124b63c9251d24de268f9984b626bc9e3b05e
dennismuturia <dmuturia2922@gmail.com>
2018-06-06 10:56:57 +0300

    Successfully added navbar to the application successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule, MDBBootstrapModule.forRoot()
